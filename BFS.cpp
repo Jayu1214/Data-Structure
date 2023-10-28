6836512dd78f347371d7facdcd294cdb3c7cc87f
Jay Trivedi <jayutrivedi1214@gmail.com>
2023-10-28 11:04:18 +0530

    Create BFS.cpp

---
+++ BFS.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <queue>
+#include <vector>
+
+using namespace std;
+
+class DataStructure {
+};
+
+void BFS(DataStructure &ds, int startVertex) {
+}
+
+int main() {
+    DataStructure ds;
+
+    int startVertex;
+    cout << "Enter the starting vertex: ";
+    cin >> startVertex;
+
+    BFS(ds, startVertex);
+
+    return 0;
+}
